72d94d4deffac5940e90acdc5de43603897d6ecb
matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>
2024-02-23 14:42:11 +0700

    feat: init project

--- dist/types/codegen/cosmos/accesscontrol_x/tx.amino.d.ts
+++
@@ -1,8 +0,0 @@
-import { MsgRegisterWasmDependency } from "./tx";
-export declare const AminoConverter: {
-    "/cosmos.accesscontrol_x.v1beta1.MsgRegisterWasmDependency": {
-        aminoType: string;
-        toAmino: (message: MsgRegisterWasmDependency) => import("./tx").MsgRegisterWasmDependencyAmino;
-        fromAmino: (object: import("./tx").MsgRegisterWasmDependencyAmino) => MsgRegisterWasmDependency;
-    };
-};
--- dist/types/codegen/gogoproto/bundle.js
+++
@@ -1,4 +0,0 @@
-import * as _183 from "./gogo";
-export const gogoproto = {
-    ..._183
-};
